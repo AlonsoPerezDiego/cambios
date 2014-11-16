9d443e0d853b0ab550f01b44c04b7a8827fd0025
Damián Nogueiras <damiannogueiras@gmail.com>
2014-11-16 17:51:52 +0100

    Inicio. Método UNO.

---
+++ Principal.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author damian
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+    System.out.println("Hola mundo");
+  }
+    // Método 1
+    public void uno(){
+        System.out.println("UNO");
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ cambios
 =======
 
 Revertir y resetear commits
+
+[Tutorial Deshaciendo cambios](https://www.atlassian.com/git/tutorials/undoing-changes)
