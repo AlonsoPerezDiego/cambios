f7d098d6a10f81306508f846ae9cfe32cd00cf55
Damián Nogueiras <damiannogueiras@gmail.com>
2015-01-22 07:13:26 +0100

    antes del merge

--- Principal.java
+++ Principal.java
@@ -2,22 +2,42 @@
  *
  * @author damian
  */
+
+/*
+ * Creación de nueva característica
+ */
 public class Principal {
     
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
     System.out.println("Hola mundo");
   }
     // Método 1
-    public void uno(){
+
+    /**
+     *
+     */
+        public void uno(){
         System.out.println("UNO");
     }
     // Método 2
-    public void dos(){
+
+    /**
+     *
+     */
+        public void dos(){
         System.out.println("DOS");
     }
-    // el TRE lo borré realizando un revert
+    // el TRES lo borré realizando un revert
     // Método 4
-    public void cuatro(){
+
+    /**
+     *
+     */
+        public void cuatro(){
         System.out.println("CUATRO");
     }
 }
